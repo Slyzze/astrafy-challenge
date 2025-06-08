e7d65cba6cf110edbbabbe230ada4d90cd4d25e7
Slyzze <93248451+Slyzze@users.noreply.github.com>
2025-06-08 20:25:52 +0200

    Delete stg_sales.sql

--- stg_sales.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  CAST(order_id AS STRING)       AS order_id,
-  CAST(products_id AS STRING)    AS products_id,
-  CAST(customer_id AS STRING)    AS customer_id,
-  CAST(net_sales AS FLOAT64)     AS net_sales,
-  CAST(qty AS INT64)             AS qty,
-  CAST(date_date AS DATE)        AS date_date
-FROM
-  {{ source('astrafy', 'Sales') }}
