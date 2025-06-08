61928fada1f7167ac976d1b9f884a061e33fe54d
Slyzze <93248451+Slyzze@users.noreply.github.com>
2025-06-08 20:25:42 +0200

    Delete stg_orders.sql

--- stg_orders.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  CAST(orders_id AS STRING)       AS orders_id,
-  CAST(customers_id AS STRING)   AS customers_id,
-  CAST(date_date AS DATE)        AS date_date
-FROM
-  {{ source('astrafy', 'Orders') }}
